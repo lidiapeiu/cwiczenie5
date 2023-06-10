482018d30ccaebdcc861e0443ec568c127e3aa58
lidiapeiu <118052935+lidiapeiu@users.noreply.github.com>
2023-06-10 13:47:32 +0200

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,12 @@
-const app = require("./app");
+const app = require("./app")
 const supertest = require("supertest")
 const request = supertest(app)
 
-discribe("/test endpoint", () => {
-	it("Powinna zwrócić się odpowiedź", async() => {
-	const response = await request.get(".test")
-	expect(response.status).toBe(200);
-	expect(response.test).toBe("Hello World"); 
-})
+describe("/test endopint", () => {
+	it("Aplikacja powinna zwrócić odpowiedź", async() => {
+	const response = await request.get("/test")
+	expect(response.status).toBe(200)
+	expect(response.text).toBe("Hello World")
 
+})
 })
